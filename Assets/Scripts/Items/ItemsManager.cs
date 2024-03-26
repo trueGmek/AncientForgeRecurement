658e6d98dda459699242044b83a0a71e249222f4
trueGmek <gmek0204@gmail.com>
2024-03-26 20:03:12 +0100

    Optimize Items Manager

--- Assets/Scripts/Items/ItemsManager.cs
+++ Assets/Scripts/Items/ItemsManager.cs
@@ -1,8 +1,10 @@
-﻿namespace AFSInterview.Items
-{
-    using TMPro;
-    using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
+namespace AFSInterview.Items
+{
     public class ItemsManager : MonoBehaviour
     {
         [SerializeField]
@@ -23,11 +25,10 @@
         [SerializeField]
         private float itemSpawnInterval;
 
-        private float nextItemSpawnTime;
 
         private void Update()
         {
-            if (Time.time >= nextItemSpawnTime)
+            if (Time.time >= _nextItemSpawnTime)
                 SpawnNewItem();
 
             if (Input.GetMouseButtonDown(0))
@@ -36,15 +37,16 @@
             if (Input.GetKeyDown(KeyCode.Space))
                 inventoryController.SellAllItemsUpToValue(itemSellMaxValue);
 
-            FindObjectOfType<TextMeshProUGUI>().text = "Money: " + inventoryController.Money;
+            _textMeshProUGUI.Value.text = $"Money: {inventoryController.Money.ToString()}";
         }
+
 
         private void SpawnNewItem()
         {
-            nextItemSpawnTime = Time.time + itemSpawnInterval;
+            _nextItemSpawnTime = Time.time + itemSpawnInterval;
 
-            var spawnAreaBounds = itemSpawnArea.bounds;
-            var position = new Vector3(
+            Bounds spawnAreaBounds = itemSpawnArea.bounds;
+            Vector3 position = new(
                 Random.Range(spawnAreaBounds.min.x, spawnAreaBounds.max.x),
                 0f,
                 Random.Range(spawnAreaBounds.min.z, spawnAreaBounds.max.z)
@@ -55,16 +57,24 @@
 
         private void TryPickUpItem()
         {
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            var layerMask = LayerMask.GetMask("Item");
-            if (!Physics.Raycast(ray, out var hit, 100f, layerMask) ||
-                !hit.collider.TryGetComponent<IItemHolder>(out var itemHolder))
+            Ray ray = _lazyCamera.Value.ScreenPointToRay(Input.mousePosition);
+
+            if (!Physics.Raycast(ray, out RaycastHit hit, 100f, _layerMask) ||
+                !hit.collider.TryGetComponent(out IItemHolder itemHolder))
                 return;
 
-            var item = itemHolder.GetItem(true);
+            Item item = itemHolder.GetItem(true);
             inventoryController.AddItem(item);
-            Debug.Log("Picked up " + item.Name + " with value of " + item.Value + " and now have " +
-                      inventoryController.ItemsCount + " items");
+
+            Debug.Log(
+                $"Picked up {item.Name} with value of {item.Value.ToString()} and now have {inventoryController.ItemsCount.ToString()} items");
         }
+
+
+        private readonly Lazy<TextMeshProUGUI> _textMeshProUGUI = new(FindObjectOfType<TextMeshProUGUI>);
+        private readonly Lazy<Camera> _lazyCamera = new(Camera.main);
+        private readonly int _layerMask = LayerMask.GetMask("Item");
+
+        private float _nextItemSpawnTime;
     }
 }
