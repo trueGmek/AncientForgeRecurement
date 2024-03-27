f52c3cd619cad7abaa3b34c8458f05d35bc2d967
trueGmek <gmek0204@gmail.com>
2024-03-27 13:33:32 +0100

    Moved TestEnumeratorFactory to correct namespace

--- Assets/Scripts/Combat/CombatManagement/TestEnumeratorFactory.cs
+++ Assets/Scripts/Combat/CombatManagement/TestEnumeratorFactory.cs
@@ -1,55 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using AFSInterview.Combat;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Ancient Forge/Combat/Test enumerator factory")]
-public class TestEnumeratorFactory : AbstractCombatEnumeratorFactory
+namespace AFSInterview.Combat
 {
-    public override IEnumerator<Unit> Get(List<Unit> units)
+    [CreateAssetMenu(menuName = "Ancient Forge/Combat/Test enumerator factory")]
+    public class TestEnumeratorFactory : AbstractCombatEnumeratorFactory
     {
-        units.Shuffle();
-        return new CircularEnumerator(units);
-    }
-
-    private class CircularEnumerator : IEnumerator<Unit>
-    {
-        private readonly List<Unit> _list;
-        private int _index;
-
-        public CircularEnumerator(List<Unit> list)
+        public override IEnumerator<Unit> Get(List<Unit> units)
         {
-            _list = list;
+            units.Shuffle();
+            return new CircularEnumerator(units);
         }
 
-        public bool MoveNext()
+        private class CircularEnumerator : IEnumerator<Unit>
         {
-            for (int i = _index + 1; i < _list.Count; i++)
-            {
-                if (_list[i].IsDead())
-                    continue;
+            private readonly List<Unit> _list;
+            private int _index;
 
-                _index = i;
+            public CircularEnumerator(List<Unit> list)
+            {
+                _list = list;
+            }
+
+            public bool MoveNext()
+            {
+                for (int i = _index + 1; i < _list.Count; i++)
+                {
+                    if (_list[i].IsDead())
+                        continue;
+
+                    _index = i;
+                    return true;
+                }
+
+                Reset();
                 return true;
             }
 
-            Reset();
-            return true;
-        }
+            public void Reset()
+            {
+                _list.RemoveAll(t => t.IsDead());
+                _index = 0;
+            }
 
-        public void Reset()
-        {
-            _list.RemoveAll(t => t.IsDead());
-            _index = 0;
-        }
+            public Unit Current => _list[_index];
 
-        public Unit Current => _list[_index];
+            object IEnumerator.Current => Current;
 
-        object IEnumerator.Current => Current;
-
-        public void Dispose()
-        {
-            _index = -1;
+            public void Dispose()
+            {
+                _index = -1;
+            }
         }
     }
 }
